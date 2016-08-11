fe05ca92cd0dc128731ba2075c7c4f865e182b96
JBYoshi <jbyoshi@users.noreply.github.com>
2016-08-11 09:43:29 -0500

    Version fix

--- src/main/java/jbyoshi/sponge/pickaxe/PickaxePlugin.java
+++ src/main/java/jbyoshi/sponge/pickaxe/PickaxePlugin.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2016 JBYoshi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package jbyoshi.sponge.pickaxe;
 
 import jbyoshi.sponge.pickaxe.command.CommandHelper;
@@ -19,9 +40,12 @@ import org.spongepowered.api.text.format.TextColors;
 import javax.inject.Inject;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
-@Plugin(id = "pickaxe", name = "Pickaxe", version = "1.0-SNAPSHOT")
+@Plugin(id = "pickaxe", name = "Pickaxe", version = "1.0",
+        description = "A Sponge plugin manager and unofficial Ore client.", authors = "JBYoshi",
+        url = "https://ore-staging.spongepowered.org/JBYoshi/Pickaxe")
 public final class PickaxePlugin {
     private static final OreRepository ORE = new CombinedOreRepository(
             new OreRepositoryImpl("https://ore-staging.spongepowered.org"),
@@ -35,9 +59,8 @@ public final class PickaxePlugin {
     public void initialize(GameInitializationEvent event) {
         installer = new PluginInstaller(Sponge.getPluginManager()
                 .fromInstance(this).orElseThrow(() -> new AssertionError("Apparently I'm not a plugin?!"))
-                .getSource().map(Path::getParent).orElseGet(() -> {
-                    // It can be absent during development. Try reflection to
-                    // access the Sponge configuration.
+                .getSource().filter(Files::isRegularFile).map(Path::getParent).orElseGet(() -> {
+                    // Try reflection to access the Sponge configuration.
                     try {
                         return (Path) Class.forName("org.spongepowered.common.launch.SpongeLaunch")
                                 .getDeclaredMethod("getPluginsDir").invoke(null);
